568bbade45100acb2d5f4175141acd8074444207
Trevor <trevorward001@gmail.com>
2024-06-23 18:17:45 -0600

    feat: added templating and simple ui using tailwind cdn

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import argparse
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse 
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 from settings import settings
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,16 +23,11 @@ async def read_index():
     return FileResponse('index.html')
 
 @app.post('/blogs')
-async def sample_blog():
-    data = '''
-    <ul>
-        <li>Sample Post 1</li>
-        <li>Sample Post 2</li>
-        <li>Sample Post 3</li>
-        <li>Sample Post 4</li>
-    </ul>
-    '''
-    return Response(content=data, media_type='text/html')
+async def sample_blog(request: Request):
+
+    return templates.TemplateResponse(
+        request=request, name="list.html"
+    ) 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the server.")
